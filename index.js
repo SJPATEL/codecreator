c1b78021f84f6659f336098328afa7cae59b3822
sjpatel <sjpatel298@gmail.com>
2023-07-21 18:17:34 +0530

    changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config({ path: './config.env' });
 require('./db/conn');
  
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT  || 5000;
 app.use(cors());
 
 app.use(express.json());
